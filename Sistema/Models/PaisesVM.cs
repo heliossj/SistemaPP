bbffa234e65775965ca683ce0ec430130c461202
Helio <helio_ss_junior@hotmail.com>
2021-02-05 08:18:38 -0300

    Inicio do projeto

---
+++ Sistema/Models/PaisesVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Models
+{
+    public class PaisesVM
+    {
+        public int? idPais { get; set; }
+        public string nmPais { get; set; }
+        public string DDI { get; set; }
+        public string sigla { get; set; }
+    }
+}
